14ee6fb6baa2e21b2f33c39063d716d06c5a76c7
Navaneetha-25 <navaneethac2006@gmail.com>
2025-09-19 15:56:12 +0530

    Add only components, App.jsx, and App.css

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import './App.css'
+import BlogList from "./components/BlogList";
+export default function App() {
+    return (
+        <div className="app-background min-h-screen p-6">
+            <BlogList />
+        </div>
+    );
+}
